fa8ec2b3c242ce39f1938bee124223d5a93a69b5
Rodrigo <rod_jplocko@hotmail.com>
2024-11-28 21:37:59 -0300

    Criação de arquivo para teste

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+"use client"
+
+import { useState } from "react"
+
+const Botao = () => {
+
+  const [contagem, set] = useState(0)
+  const [cancelado, setCancelado] = useState(false)
+  const handleClickA = () => set(contagem + 1) 
+  const handleClickD = () => set(contagem - 1)
+  const handleCancel= () => {
+    const confirmation = window.confirm("Deseja realmente cancelar?");
+    if (confirmation) {
+      setCancelado(true);
+        alert("Cancelado com sucesso");
+    } else {
+      alert("Não cancelado.");
+    }
+  }
+
+  return(
+
+    <div>
+      <p className="m-1">{contagem}</p>
+
+      <button onClick={handleClickA} className="m-1 p-1 bg-gray-900 text-white rounded-md">+</button>
+
+      <button onClick={handleClickD} className="m-1 p-1 bg-gray-900 text-white rounded-md">-</button>
+
+      <button onClick={handleCancel} className="m-1 p-1 bg-gray-900 text-white rounded-md">Cancelar</button>
+
+
+    </div>
+  )
+}
+
+export default Botao
